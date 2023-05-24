069b4ede4e3ee1da029e057cf27bbaebafb35ec0
Julian De Angelis <julian.deangelis@gmail.com>
2023-05-24 17:36:26 -0400

    Create unzipping... screen

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +17,19 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCZr-87hJ3M-j0svyTk5and1C7swKND5nI",
+  authDomain: "julianzip.firebaseapp.com",
+  projectId: "julianzip",
+  storageBucket: "julianzip.appspot.com",
+  messagingSenderId: "1026793584334",
+  appId: "1:1026793584334:web:cedc9a2fefb321306d029f",
+  measurementId: "G-5ZD4PS3NHS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
