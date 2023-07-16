2dffcbd88ab2ae685d5bcb8663d313c7b0144cf0
Julian De Angelis <julian.deangelis@gmail.com>
2023-07-15 20:23:45 -0400

    Create and style main.js
    - Create Image and Link styles

--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,39 @@
 import '../styles/App.css'
+import { Grid } from '@mui/material'
+import github from '../media/github-mark.png'
 
 const Main = () => {
 
     return(
-        <div className='Header1'>
-            main
+        <div>
+            <Grid container rowSpacing={1}>
+                <Grid item xs={12}>
+                    <div className='Header1'>
+                        Julian
+                    </div>
+                </Grid>
+                <Grid item xs={12}>
+                    <div className='Header1'>
+                        De Angelis
+                    </div>
+                </Grid>
+                <Grid item xs={12}>
+                    <div className='Header2'>
+                        Software Engineer
+                    </div>   
+                </Grid>
+                <Grid item xs={12}>
+                    <div className='Bodycopy'>
+                        <span style={{ marginRight: 17 }}>
+                            julian.deangelis@gmail.com
+                        </span>
+                        <img className='Image' src={github} alt='GitHub Logo'/> 
+                        <a className='Link' href='https://github.com/juliandeange' target='_blank' rel="noreferrer">
+                            juliandeange
+                        </a>
+                    </div>
+                </Grid>
+            </Grid>
         </div>
     )
 
