672c01b157184a65b7ed8bd7fbb1f94688134249
Julian De Angelis <julian.deangelis@gmail.com>
2023-07-16 17:31:54 -0400

    Add first set of employment data
    - Add projects to js file
    - Create SubheadingData style

--- src/components/work.js
+++ src/components/work.js
@@ -1,12 +1,51 @@
 import '../styles/App.css'
-import { WorkData1, WordData2, WordData3, WordData4, WordData5 } from '../data/employment'
+import { 
+    WorkData1,
+    WorkData2,
+    WorkData3,
+    WorkData4,
+    WorkData5
+} from '../data/employment'
+import {
+    Covontario1,
+    Covontario2,
+    Criptoz,
+    CrowdControl
+} from '../data/projects'
 
 const Work = () => {
 
     return(
-        <div>
-            <div className='Bodycopy'>
-                
+        <div style={{ maxWidth: '50%' }}>
+            <div className='Header2'>
+                Work
+            </div>
+            <div className='Subheading' style={{ marginTop: 10, marginBottom: 10 }}>
+                Software Engineer | MDA
+            </div>
+            <div className='SubheadingDate'>
+                Jan 2018 - Present
+            </div>
+            <div className='Bodycopy' style={{ marginTop:30 }}>
+                {WorkData1}
+                <br />
+                <br />
+                {WorkData2}
+                <br />
+                <br />
+                {WorkData3}
+                <br />
+                <br />
+                {WorkData4}
+                <br />
+                <br />
+                {WorkData5}
+            </div>
+            <div className='Header2' style={{ marginTop: 40 }}>
+                Projects
+            </div>
+            <div className='Subheading' style={{ marginTop: 10, marginBottom: 10 }}>
+                Covontario.ca
             </div>
         </div>
     )
---
+++ src/data/projects.js
@@ -0,0 +1,6 @@
+export const Covontario1 = 'Provides a graph that tracks the change in Ontario’s COVID-19 statistics. Displays the change and totals for active cases, new cases, recoveries, tests, hospitalizations and mortality.'
+export const Covontario2 = 'Data collection is collected through a node/ruby script deployed on a Raspberry Pi 4 where it is scheduled to run daily. Latest statistics are automatically tweeted to a Twitter account with an audience of over 200.'
+
+export const Criptoz = 'Fetches and displays the current price of many cryptocurrencies. Gives the options to view the prices in multiple currencies.'
+
+export const CrowdControl = 'Provides an easy to use interface for multiple users to keep a live count of people in a building. All data is stored inside a firestore database and includes the ability to create new stores and set a capacity for each.'
