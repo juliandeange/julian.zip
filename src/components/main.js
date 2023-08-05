7ee12d3d66520a27b135a71f15fffc978fd8656c
Julian De Angelis <julian.deangelis@gmail.com>
2023-08-05 09:54:13 -0400

    Remove mui Grid layout from main.js

--- src/components/main.js
+++ src/components/main.js
@@ -1,39 +1,29 @@
 import '../styles/App.css'
-import { Grid } from '@mui/material'
 import github from '../media/github-mark.png'
 
 const Main = () => {
 
     return(
-        <div>
-            <Grid container rowSpacing={1}>
-                <Grid item xs={12}>
-                    <div className='Header1'>
-                        Julian
-                    </div>
-                </Grid>
-                <Grid item xs={12}>
-                    <div className='Header1'>
-                        De Angelis
-                    </div>
-                </Grid>
-                <Grid item xs={12}>
-                    <div className='Header2'>
-                        Software Engineer
-                    </div>   
-                </Grid>
-                <Grid item xs={12}>
-                    <div className='Bodycopy'>
-                        <span style={{ marginRight: 17 }}>
-                            julian.deangelis@gmail.com
-                        </span>
-                        <a className='Link' href='https://github.com/juliandeange' target='_blank' rel="noreferrer">
-                            <img className='Image' src={github} alt='GitHub Logo'/> 
-                            juliandeange
-                        </a>
-                    </div>
-                </Grid>
-            </Grid>
+        <div style={{ maxWidth: '50%' }}>
+
+            <div className='Header1'>
+                Julian
+            </div>
+            <div className='Header1'>
+                De Angelis
+            </div>
+            <div className='Header2' style={{ marginTop: 20 }}>
+                Software Engineer
+            </div>
+            <div className='Bodycopy' style={{ marginTop: 10 }}>
+                <span style={{ marginRight: 17 }}>
+                    julian.deangelis@gmail.com
+                </span>
+                <a className='Link' href='https://github.com/juliandeange' target='_blank' rel="noreferrer">
+                    <img className='Image' src={github} alt='GitHub Logo'/> 
+                    juliandeange
+                </a>
+            </div>
         </div>
     )
 
