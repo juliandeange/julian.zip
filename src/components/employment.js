99f0cca6dc3e3133b113cb17e478c3c21964a94f
Julian De Angelis <julian.deangelis@gmail.com>
2024-04-02 18:01:50 -0400

    Parse data structure for employment

--- src/components/employment.js
+++ src/components/employment.js
@@ -1,49 +1,36 @@
 import '../styles/App.css'
-import { 
-    WorkData1,
-    WorkData2,
-    WorkData3,
-    WorkData4,
-    WorkData5
-} from '../data/data-employment'
-import {
-    Covontario1,
-    Covontario2,
-    Criptoz,
-    CrowdControl
-} from '../data/data-projects'
+
+import { employmentData } from '../data/data-employment'
 
 const Employment = () => {
 
     return(
-        <div style={{ maxWidth: '50%' }}>
-            <div className='Header2'>
-                Work
-            </div>
-            <div className='Subheading' style={{ marginTop: 10, marginBottom: 10 }}>
-                Software Engineer | MDA
-            </div>
-            <div className='SubheadingDate'>
-                Jan 2018 - Present
-            </div>
-            <div className='Bodycopy' style={{ marginTop:20 }}>
-                {WorkData1}
-                <br />
-                <br />
-                {WorkData2}
-                <br />
-                <br />
-                {WorkData3}
-                <br />
-                <br />
-                {WorkData4}
-                <br />
-                <br />
-                {WorkData5}
-            </div>
+        <div style={{ maxWidth: '60%' }}>
+            {employmentData.map((data) => {
+                return (
+                    <div>
+                        <div className='Subheading' style={{ marginTop: 10, marginBottom: 10 }}>
+                            {data.position} | {data.company}
+                        </div>
+                        <div className='SubheadingDate'>
+                            {data.dates}
+                        </div>
+                        <div className='Bodycopy' style={{ marginTop:20 }}>
+                            {data.responsibilites.map((responsibility) => {
+                                return (
+                                    <div>
+                                        {responsibility}
+                                        <br />
+                                        <br />
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                )
+            })}
         </div>
     )
-
 }
 
 export default Employment
