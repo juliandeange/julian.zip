c7a3540df5e18a2f3504a1c59b847842b58815f7
Julian De Angelis <julian.deangelis@gmail.com>
2024-04-02 17:56:59 -0400

    Create object for employment

--- src/data/data-employment.js
+++ src/data/data-employment.js
@@ -1,5 +1,27 @@
-export const WorkData1 = 'Developed various large scale engineering applications in an agile environment used for analyzing parts and telemetry downloaded from the International Space Station.'
-export const WorkData2 = 'Designed a user interface allowing an operator to send and receive commands and telemetry for robotic components deployed in-orbit or a virtual environment.'
-export const WorkData3 = 'Using the Unity game engine, developed a virtual simulator for visualizing 3D objects based on inbound telemetry from an external interface over a UDP connection.'
-export const WorkData4 = 'Created a standardized React/C# development environment for future ground station software systems along with an automated continuous integration and unit testing pipeline.'
-export const WorkData5 = 'Designed the ground and flight control software of a centrifuge which can be remotely commanded by a scripting engine for in-orbit operations aboard the International Space Station.'
+export const employmentData = [
+    {
+        company: 'Intuit',
+        position: 'Sotware Engineer 2',
+        dates: 'Nov 2023 - Present',
+        location: 'Toronto, Ontario',
+        responsibilites: [
+            'Develop, maintain, deploy, and load test large-scale web applications which enable users to import tax documents seamlessly across various platforms.',
+            'Improve performance metrics of React applications to optimize loading times between pages, driving customer interactions and engagements.',
+            'Align with product managers and customers to better understand implementation details of tax software applications leading to improved user experiences.'
+        ]
+    },
+    {
+        company: 'MDA',
+        position: 'Software Engineer',
+        dates: 'Jan 2018 - Oct 2023',
+        location: 'Brampton, Ontario',
+        responsibilites: [
+            'Designed the ground and flight control software of a centrifuge which can be remotely commanded by a scripting engine for in-orbit operations aboard the International Space Station.',
+            'Created a standardized React/C# development environment for future ground station software systems along with an automated continuous integration and unit testing pipeline.',
+            'Using the Unity game engine, developed a virtual simulator for visualizing 3D objects based on inbound telemetry from an external interface over a UDP connection.',
+            'Designed a user interface allowing an operator to send and receive commands and telemetry for robotic components deployed in-orbit or a virtual environment.',
+            'Developed various large scale engineering applications in an agile environment used for analyzing parts and telemetry downloaded from the International Space Station.'
+        ]
+    }
+]
+
